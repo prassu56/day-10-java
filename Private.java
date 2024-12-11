030d8cd7a1b70416d09e6b2e7dd40a920a8d91b3
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-11 10:42:12 +0530

    Add files via upload

---
+++ Private.java
@@ -0,0 +1,10 @@
+class Private{
+    private static void s()
+    {
+        System.out.println("hello students");
+    }
+  
+        public static void main(String[]args){
+            s();
+        }
+    }
