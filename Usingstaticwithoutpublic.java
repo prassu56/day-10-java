2079191d202ff2540b766348b37ce6c68a8b8fdf
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-11 11:07:46 +0530

    Add files via upload

---
+++ Usingstaticwithoutpublic.java
@@ -0,0 +1,12 @@
+class Usingstaticwithoutpublic{
+
+     static String s()
+    {
+        return "hello students";
+    }
+    public static void main(String[]args){
+        String res=s();
+
+        System.out.println(res);
+    }
+}
