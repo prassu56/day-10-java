6ae86db2085ea3fcb5b57895c8cae7156c4d21a6
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-11 11:04:22 +0530

    Add files via upload

---
+++ Protected.java
@@ -0,0 +1,10 @@
+class Protected{
+    protected static void s()
+    {
+        System.out.println("hello students");
+    }
+  
+        public static void main(String[]args){
+            s();
+        }
+    }
